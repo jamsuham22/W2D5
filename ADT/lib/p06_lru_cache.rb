abdb5a200e8fce27df3e52767e19a33d54b29719
App Academy Student <brothman7000@gmail.com>
2018-08-03 17:08:00 -0400

    LRU Cache

--- ADT/lib/p06_lru_cache.rb
+++ ADT/lib/p06_lru_cache.rb
@@ -1,5 +1,7 @@
 require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
+require 'byebug'
+
 
 class LRUCache
   attr_reader :count
@@ -15,6 +17,21 @@ class LRUCache
   end
 
   def get(key)
+    return_val = nil
+    if @map.include?(key)
+      update_node!(@map[key])
+      return_val = @map[key].val
+    else
+      val = calc!(key)
+      # debugger
+      @store.append(key, val)
+      @map[key] = @store.last
+      return_val = val
+    end
+
+    eject! if count > @max
+
+    return_val
   end
 
   def to_s
@@ -25,12 +42,19 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    @prc.call(key)
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    node.remove
+    @store.append(node.key, node.val)
+    @map[node.key] = @store.last
   end
 
   def eject!
+    least_used_node = @store.first
+    least_used_node.remove
+
+    @map.delete(least_used_node.key)
   end
 end
